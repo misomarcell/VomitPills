4c72ef9e4a56aeb05f29d6d8c6391c5f02ea7d14
misomarcell <misomarcell@gmail.com>
2017-03-20 22:06:01 +0100

    init

---
+++ src/Subscribe.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Subscribe")
+public class Subscribe extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         
+        String email = request.getParameter("email");      
+        System.out.println(email);
+        
+        PrintWriter writer = response.getWriter();      
+        String htmlRespone = "<html><h2>You successfully subscribed to the newsletter! ( " + email + " )</h2>";
+        htmlRespone += "<a href='index.html'>Go back...</a></html>";
+        writer.println(htmlRespone);
+       
+    }
+
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
+		System.out.println("Started...");
+	}
+	
+}
