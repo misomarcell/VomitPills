f3087015525e398389588cf256e6b0ae111d58a4
misomarcell <misomarcell@gmail.com>
2017-03-25 00:50:06 +0100

    Fixed the bug with HTML tags. This is important.

--- src/Messages.java
+++ src/Messages.java
@@ -11,26 +11,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class SendMessage
- */
 @WebServlet("/feedback")
 public class Messages extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	private List<Message> messages = new ArrayList<Message>();
 	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Messages() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	   		
 		String formedMessages = "";    
@@ -51,7 +42,7 @@ public class Messages extends HttpServlet {
 	    			"<div class=\"single-message\">" + 
 	    			dateString +
 	    			"<b>" + message.getAuthor() + "</b>: " +
-	    			message.getContent() +    			
+	    			message.getContent().replace("<", "&lt;") +  			
 	    			"</div>";
 		}
 	    
@@ -61,9 +52,6 @@ public class Messages extends HttpServlet {
 	    request.getRequestDispatcher("/feedback.jsp").forward(request, response);    
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String message = request.getParameter("message");    
 		String name = request.getParameter("name"); 
